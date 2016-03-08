13006f923a10d022452a84fefe7c870a17ab715a
rockclimber112358 <rockclimber112358@gmail.com>
2016-03-08 15:49:56 +0100

    Added a few new submissions

--- Python/NCAA Scripts/make_submission.py
+++ Python/NCAA Scripts/make_submission.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 def make_submission(data, filename = None):
-    submission = pd.DataFrame(data['Year'] + '_' + data['team_id1'] + '_' + data['team_id2'])
-    submission['Pred'] = data['Pred']
+    # Don't want to modify data, so take copy
+    d = data.copy()
+    d['Year'] = d['Year'].astype(str)
+    d['team_id1'] = d['team_id1'].astype(str)
+    d['team_id2'] = d['team_id2'].astype(str)
+    submission = pd.DataFrame(d['Year'] + '_' + d['team_id1'] + '_' + d['team_id2'])
+    submission['Pred'] = d['Pred']
     submission.columns = ['Id', 'Pred']
     if filename is not None:
         submission.to_csv('NCAA Submissions/' + filename, index = False)
